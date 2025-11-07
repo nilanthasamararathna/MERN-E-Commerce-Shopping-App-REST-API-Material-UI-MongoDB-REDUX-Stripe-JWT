d231eb913e495fa1ccb978aacd27503bdce55e86
E.Nilantha Samararathna <45939807+nilanthasamararathna@users.noreply.github.com>
2025-11-07 08:42:12 +0530

    Create react app, Navbar, Hero Slider and Categories and Product Sections

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import { Search, ShoppingCartOutlined } from '@mui/icons-material'
+import { Badge } from '@mui/material'
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 60px;
+
+`
+const Wrapper = styled.div`
+    padding:10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+const Left = styled.div`
+    display: flex;
+    flex:1;
+    align-items: center;
+`
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+const SearchContainer = styled.div`
+    border:1px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+const Input = styled.input`
+    border: none;
+    &:focus {
+        outline: none;
+  }
+    
+`
+const Center = styled.div`
+  text-align: center;
+    flex:1;
+`
+const Logo = styled.h1`
+    font-weight: bold;
+`
+const Right = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    flex:1;
+`
+const MenuItem = styled.div`
+    font-style: 14px;
+    margin-left: 25px;
+    cursor: pointer;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+        <Left><Language>EN</Language>
+        <SearchContainer>
+            <Input/>
+            <Search style={{color:"gray", fontSize:16}}/>
+        </SearchContainer>
+        </Left>
+        <Center><Logo>ESHOP.</Logo></Center>
+        <Right>
+            <MenuItem>REGISTER</MenuItem>
+            <MenuItem>SIGN IN</MenuItem>
+            <MenuItem>
+            <Badge badgeContent={4} color="primary">
+            <ShoppingCartOutlined />
+</Badge>
+            </MenuItem>
+        </Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
